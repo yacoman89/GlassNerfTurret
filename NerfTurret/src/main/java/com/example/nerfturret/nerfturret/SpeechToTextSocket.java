bfda8ea6e123351dc37b80e2fca47efb85968b69
Lloyd Ramey <lnr0626@gmail.com>
2014-04-19 16:54:44 -0400

    Discover host stuff working

--- NerfTurret/src/main/java/com/example/nerfturret/nerfturret/SpeechToTextSocket.java
+++ NerfTurret/src/main/java/com/example/nerfturret/nerfturret/SpeechToTextSocket.java
@@ -20,24 +20,38 @@ public class SpeechToTextSocket extends AsyncTask<ArrayList<String>,Void,Void> {
     DataInputStream dataInputStream;
     DataOutputStream dataOutputStream;
     public static final String TAG = "Socket for voice";
+    public static final int VOICE_CHANGER_PORT = 6789;
+
+    private String host;
+    private boolean serverPresent;
+
+    public SpeechToTextSocket(boolean serverPresent, String serverHost) {
+        this.host = serverHost;
+        this.serverPresent = serverPresent;
+    }
 
     @Override
     protected Void doInBackground(ArrayList<String>... params) {
-//        try {
-//            socket = new Socket("192.168.1.1", 6789);
-//            dataInputStream = new DataInputStream(socket.getInputStream());
-//            dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-            for(int i=0; i<params[0].size(); i++) {
-                String text = params[0].get(i);
-
-                Log.i(TAG, text);
-                //dataOutputStream.writeBytes(text);
+        try {
+            if(serverPresent) {
+                socket = new Socket(host, VOICE_CHANGER_PORT);
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            } else {
+                Log.d(TAG, "Voice Changer not present -- just logging data");
             }
 
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+            String text;
+            for(int i=0; i<params[0].size(); i++) {
+                text = params[0].get(i);
+
+                Log.i(TAG, text);
+                if(serverPresent) dataOutputStream.writeBytes(text);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         return null;
